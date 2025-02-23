2a7cfa7a519fdeddc8c891435e1e635fbe1c2d37
Om Amin <omamin135@gmail.com>
2025-02-22 20:49:51 -0500

    initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
+import 'react-native-reanimated';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
